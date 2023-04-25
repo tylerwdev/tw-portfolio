d16ec0249105fe2a83c8ca525e08c025554e3973
Tyler Winters <tylerwintersdev@gmail.com>
2023-04-25 14:58:24 -0700

    added assests folder and contents

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+// *START* This is where JS goes for making css component styles work
+
+$(document).ready(function () {
+  $(".sidenav").sidenav();
+});
+
+$(document).ready(function () {
+  $(".modal").modal();
+});
+
+$(document).ready(function () {
+  $("input#input_text, textarea#textarea2").characterCounter();
+});
+
+// *END* This is where JS goes for making css component styles work
